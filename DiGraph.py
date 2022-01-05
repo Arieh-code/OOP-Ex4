93e2ac6c974a2bd0ecfb39b79ae6da7fae8bad39
Arieh Norton <arieh.norton@gmail.com>
2022-01-05 19:25:16 +0200

    Digraph

--- DiGraph.py
+++ DiGraph.py
@@ -1,20 +1,25 @@
-from src.GraphInterface import GraphInterface
-from src.Edge import *
-from src.Gnode import *
+from GraphInterface import GraphInterface
+from client_python.Edge import Edge
+from Gnode import Gnode
+
 """This class represents graph."""
 
-class DiGraph(GraphInterface):
 
+class DiGraph(GraphInterface):
     """Constructor"""
+
     def __init__(self):
         self.node_map = {}
         self.edge_map = {}
         self.mc = 0
+
     """This function returns the number of vertices in this graph. """
+
     def v_size(self) -> int:
         return len(self.node_map)
 
     """This function returns the number of edges in this graph."""
+
     def e_size(self) -> int:
         counter = 0
         for key in self.edge_map:
@@ -23,6 +28,7 @@ class DiGraph(GraphInterface):
 
     """This function returns a dictionary of all the nodes in the Graph, each node is represented using a pair
          (node_id, node_data)."""
+
     def get_all_v(self) -> dict:
         for node in self.node_map.values():
             node.edges_in = len(self.all_in_edges_of_node(node.key))
@@ -31,6 +37,7 @@ class DiGraph(GraphInterface):
 
     """This function returns a dictionary of all the nodes connected to node_id ,
         each node is represented using a pair (other_node_id, weight)."""
+
     def all_in_edges_of_node(self, id1: int) -> dict:
         in_edges = {}
         # use try in case the inputted node id does not exist.
@@ -46,6 +53,7 @@ class DiGraph(GraphInterface):
 
     """This function returns a dictionary of all the nodes connected from node_id , each node is represented using a pair
         (other_node_id, weight)"""
+
     def all_out_edges_of_node(self, id1: int) -> dict:
         out_edges = {}
         # use try in case the inputted node id does not exist.
@@ -58,10 +66,12 @@ class DiGraph(GraphInterface):
             return out_edges
 
     """This function returns the current version of this graph."""
+
     def get_mc(self) -> int:
         return self.mc
 
     """This function adds an edge to the graph. """
+
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
         #  create temp edge
         temp_edge = Edge(id1, weight, id2)
@@ -85,6 +95,7 @@ class DiGraph(GraphInterface):
             return False
 
     """This function adds a node to the graph."""
+
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
         temp_node = Gnode(node_id, pos)
         if self.node_map.get(node_id) is None:
@@ -96,6 +107,7 @@ class DiGraph(GraphInterface):
             return False
 
     """This function removes a node from the graph."""
+
     def remove_node(self, node_id: int) -> bool:
         if self.node_map.get(node_id) is None:
             print("Node {} doesn't exist ".format(node_id))
@@ -106,6 +118,7 @@ class DiGraph(GraphInterface):
             return True
 
     """This function removes an edge from the graph."""
+
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
         if self.edge_map.get(node_id1).get(node_id2) is None:
             print("Edge doesn't exist")
