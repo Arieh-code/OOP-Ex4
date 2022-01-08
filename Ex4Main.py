35c1f9601f57ee28f490906c4479b455bd65bc51
Arieh Norton <arieh.norton@gmail.com>
2022-01-08 23:01:13 +0200

    Done main

--- Ex4Main.py
+++ Ex4Main.py
@@ -1,39 +1,42 @@
-import json
-
-import pygame
-
+"""This is the Main Class"""
 from client import Client
-from time import sleep
 from GUI import GUI
-from GameFunc import *
+from GameFunc import Game
 import subprocess
 
-subprocess.Popen(['powershell.exe', f'java -jar Ex4_Server_v0.0.jar {11}'])
-# default port
-PORT = 6666
-# server host (default localhost 127.0.0.1)
-HOST = '127.0.0.1'
+subprocess.Popen(['powershell.exe', f'java -jar Ex4_Server_v0.0.jar {1}'])
 
-clock = pygame.time.Clock()
 
-client = Client()
-game = Game()
-client.start_connection(HOST, PORT)
-client.add_agent("{\"id\":0}")
-client.add_agent("{\"id\":1}")
-client.add_agent("{\"id\":2}")
-client.add_agent("{\"id\":3}")
-client.add_agent("{\"id\":4}")
-client.start()
-game.initialize(client.get_pokemons(), client.get_agents(), client.get_graph())
-print(game.algo.get_graph())
-# game.pokemon_src_dest(game.pokemons_list[0])
-gui = GUI(game, client)
+def main():
+    # default port
+    PORT = 6666
+    # server host (default localhost 127.0.0.1)
+    HOST = '127.0.0.1'
+    # initializing the Client
+    client = Client()
+    client.start_connection(HOST, PORT)
+    client.add_agent("{\"id\":0}")
+    client.add_agent("{\"id\":1}")
+    client.add_agent("{\"id\":2}")
+    client.add_agent("{\"id\":3}")
+    client.add_agent("{\"id\":4}")
+    client.start()
 
-# game.pokemon_src_dest(game.pokemons_list[0])
+    # initializing the game
+    game = Game()
+    game.initialize(client.get_pokemons(), client.get_agents(), client.get_graph())
 
-while client.is_running() == 'true':
-    game.play(client)
-    gui.draw()
-    print(client.move())
-    print(client.get_info())
+    # initializing the Gui
+    gui = GUI(game, client)
+
+    # the main while loop: The game will run while the 'is_running' function remains true:
+    while client.is_running() == 'true':
+        game.play(client)
+        print(game.pokemon_list)
+        gui.draw()
+        print(client.move())
+        print(client.get_info())
+
+
+if __name__ == '__main__':
+    main()
